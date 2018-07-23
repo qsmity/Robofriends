5c85e885df1ab9da283a2dcc885c470ba7fcc3b8
Quynn Smith <qsmity@gmail.com>
2018-07-23 03:14:10 +0100

    adding updated files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Card from './Card';
+import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import 'tachyons'; 
+ 
 
 ReactDOM.render(
-	<div>
-		<Card />
-		<Card />
-		<Card />
-	</div>
+	<App />
 	, document.getElementById('root'));
 registerServiceWorker();
